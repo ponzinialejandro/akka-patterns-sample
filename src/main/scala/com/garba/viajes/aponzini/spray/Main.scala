bdb7eb8278edc573d7b494797cd290c524f91a68
Ponzini Alejandro <ponzinialejandro@gmail.com>
2017-12-03 13:26:20 -0300

    add spray support

---
+++ src/main/scala/com/garba/viajes/aponzini/spray/Main.scala
@@ -0,0 +1,18 @@
+package com.garba.viajes.aponzini.spray
+
+import akka.actor._
+import akka.actor.{ActorSystem, Props}
+import akka.io.IO
+import spray.can.Http
+
+object Main extends App {
+
+  // we need an ActorSystem to host our application in
+  implicit val system = ActorSystem("on-spray-can")
+
+  // create and start our service actor
+  val service = system.actorOf(Props[WeatherServices], "demo-service")
+
+  // start a new HTTP server on port 8080 with our service actor as the handler
+  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
+}
