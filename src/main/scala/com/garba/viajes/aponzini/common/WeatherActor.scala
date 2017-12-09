26705dc0ceb7ce9cba8c81b2ab02fa9333256d32
Ponzini Alejandro <ponzinialejandro@gmail.com>
2017-12-09 19:21:05 -0300

    Logging messages

--- src/main/scala/com/garba/viajes/aponzini/common/WeatherActor.scala
+++ src/main/scala/com/garba/viajes/aponzini/common/WeatherActor.scala
@@ -1,9 +1,18 @@
 package com.garba.viajes.aponzini.common
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorLogging, ActorSystem}
 import akka.stream.ActorMaterializer
 
-abstract class WeatherActor extends Actor with ActorSystemContext{
+abstract class WeatherActor extends Actor with ActorSystemContext with ActorLogging {
+
+
+  override def preStart(): Unit = {
+    log.info("Starting actor {} #{}", this.getClass.getSimpleName , this.hashCode())
+  }
+
+  override def postStop(): Unit = {
+    log.info("Stopping actor {} #{}", this.getClass.getSimpleName , this.hashCode())
+  }
 
   def die = {
     println(s"DIE ${getClass.getName}")
