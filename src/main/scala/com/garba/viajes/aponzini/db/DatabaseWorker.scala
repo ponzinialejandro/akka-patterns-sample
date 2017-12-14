abe76726425243162956f9a235b62780dfd57ada
Ponzini Alejandro <aponzini@tekgenesis.com>
2017-12-14 10:08:00 -0300

    database working example

--- src/main/scala/com/garba/viajes/aponzini/db/DatabaseWorker.scala
+++ src/main/scala/com/garba/viajes/aponzini/db/DatabaseWorker.scala
@@ -2,11 +2,18 @@ package com.garba.viajes.aponzini.db
 
 import com.garba.viajes.aponzini.common.WeatherActor
 
+import scala.concurrent.Future
+
 class DatabaseWorker extends WeatherActor {
 
   override def receive = {
-    case asd =>
-      println("ENTRA al DB WORKER "+ hashCode())
+    case _ =>
+      println("DB WORKER "+ hashCode())
+      Future {
+        println("DB WORKING...")
+        Thread.sleep(2000)
+        println("Mocking saved in another thread!")
+      }
   }
 
 }
